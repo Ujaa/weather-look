2e41d52dc4f1adc052791326da0b927d212bf58a
Ujaa <yujinyll@naver.com>
2020-08-26 21:54:19 +0900

    앱 아이콘/이름 수정

--- OpenCV/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ OpenCV/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\SM-PC\\AndroidStudioProjects\\weather-look22\\OpenCV\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\yujin\\Desktop\\weather-look22\\OpenCV\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
