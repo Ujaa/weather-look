44b9e07a50b53865691f8becbc817c2ad8ce2e3d
sojeong <sojeong5879@gmail.com>
2020-09-01 22:10:13 +0900

    DetailActivity 수정

--- app/src/main/java/org/sookmyung/weatherlook/DetailActivity.java
+++ app/src/main/java/org/sookmyung/weatherlook/DetailActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
