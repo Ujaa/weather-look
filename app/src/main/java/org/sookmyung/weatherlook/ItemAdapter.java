c93000a5164522fb2d01efe409c4376f0caa1235
Ujaa <yujinyll@naver.com>
2020-09-02 21:49:09 +0900

    리드미 수정 로딩관련 삭제

--- app/src/main/java/org/sookmyung/weatherlook/ItemAdapter.java
+++ app/src/main/java/org/sookmyung/weatherlook/ItemAdapter.java
@@ -15,15 +15,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-// 기존 CategoryAdapter 수정 후 ItemAdapter로 이름 변경
-// CategoryAdapter 다른 내용으로 신규 생성
 // <희> - 하위 카테고리를 표시할 horizontal recyclerview
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
 
     private Context context;
     private ArrayList<ItemInfo> items;
-    //private ArrayList<Item> items;
-    //private View.OnClickListener onClickItem;
     private OnClickListener mListener;
 
     public interface OnClickListener{
@@ -37,23 +33,6 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     public ItemAdapter(Context context, ArrayList<ItemInfo> items) {
         this.context = context;
         this.items = items;
-        /*
-        onClickItem = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String str = (String) view.getTag();
-                int position = getAdapterPosition();
-                Intent intent = new Intent(context.getApplicationContext(), DetailActivity.class);
-
-                intent.putExtra("img", itemInfoList.get(position).imgSrc);
-                intent.putExtra("name", itemInfoList.get(position).name);
-                intent.putExtra("price", itemInfoList.get(position).price);
-
-                context.startActivity(intent);
-            }
-        };
-
-         */
     }
 
     @Override
